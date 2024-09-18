faa2808d03dd8ade8d97e881de68bdeb4fc9cf61
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2024-09-18 14:41:12 +0200

    .

--- Tutorial Defaults/Scripts/EditorLayoutChanger.cs
+++ Tutorial Defaults/Scripts/EditorLayoutChanger.cs
@@ -4,85 +4,82 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+public class EditorLayoutChanger : MonoBehaviour
 {
-    public class EditorLayoutChanger : MonoBehaviour
+    public static void SwitchProjectFolder(string path)
     {
-        public static void SwitchProjectFolder(string path)
+        if (AssetDatabase.IsValidFolder(path))
         {
-            if (AssetDatabase.IsValidFolder(path))
-            {
-                EditorUtility.FocusProjectWindow();
-                var folder = AssetDatabase.LoadAssetAtPath<Object>(path);
-                Selection.activeObject = folder;
-                Debug.Log($"Switched Project view to folder: {path}");
-            }
-            else
-            {
-                Debug.LogError($"Invalid folder path: {path}");
-            }
+            EditorUtility.FocusProjectWindow();
+            var folder = AssetDatabase.LoadAssetAtPath<Object>(path);
+            Selection.activeObject = folder;
+            Debug.Log($"Switched Project view to folder: {path}");
         }
-        
-        public static void FocusOnPrefabAsset(string path)
+        else
         {
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            if (prefab != null)
+            Debug.LogError($"Invalid folder path: {path}");
+        }
+    }
+
+    public static void FocusOnPrefabAsset(string path)
+    {
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        if (prefab != null)
+        {
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = prefab;
+            Debug.Log($"Focused on prefab: {path}");
+        }
+        else
+        {
+            Debug.LogError($"Invalid prefab path: {path}");
+        }
+    }
+
+
+    public static void FocusOnHierarchyObject(string name)
+    {
+        var allObjects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (var obj in allObjects)
+        {
+            if (obj.name == name)
             {
-                EditorUtility.FocusProjectWindow();
-                Selection.activeObject = prefab;
-                Debug.Log($"Focused on prefab: {path}");
-            }
-            else
-            {
-                Debug.LogError($"Invalid prefab path: {path}");
+                Selection.activeGameObject = obj;
+                EditorGUIUtility.PingObject(obj);
+                Debug.Log($"Focused on GameObject: {name}");
+                return;
             }
         }
 
+        Debug.LogError($"Object with name {name} not found in hierarchy.");
+    }
 
-        public static void FocusOnHierarchyObject(string name)
+    public static void ClearHierarchySearchField()
+    {
+        var hierarchyWindowType = typeof(EditorWindow).Assembly.GetType("UnityEditor.SceneHierarchyWindow");
+        if (hierarchyWindowType != null)
         {
-            var allObjects = GameObject.FindObjectsOfType<GameObject>();
-            foreach (var obj in allObjects)
+            var hierarchyWindow = EditorWindow.GetWindow(hierarchyWindowType);
+            if (hierarchyWindow != null)
             {
-                if (obj.name == name)
+                var searchableWindow = typeof(SearchableEditorWindow);
+                var setSearchMethod =
+                    searchableWindow.GetMethod("SetSearch", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                if (setSearchMethod != null)
                 {
-                    Selection.activeGameObject = obj;
-                    EditorGUIUtility.PingObject(obj);
-                    Debug.Log($"Focused on GameObject: {name}");
-                    return;
+                    setSearchMethod.Invoke(hierarchyWindow, new object[] {""});
+                    Debug.Log("Hierarchy search field cleared.");
+                }
+                else
+                {
+                    Debug.LogError("Unable to find SetSearch method.");
                 }
             }
-
-            Debug.LogError($"Object with name {name} not found in hierarchy.");
         }
-
-        public static void ClearHierarchySearchField()
+        else
         {
-            var hierarchyWindowType = typeof(EditorWindow).Assembly.GetType("UnityEditor.SceneHierarchyWindow");
-            if (hierarchyWindowType != null)
-            {
-                var hierarchyWindow = EditorWindow.GetWindow(hierarchyWindowType);
-                if (hierarchyWindow != null)
-                {
-                    var searchableWindow = typeof(SearchableEditorWindow);
-                    var setSearchMethod =
-                        searchableWindow.GetMethod("SetSearch", BindingFlags.Instance | BindingFlags.NonPublic);
-
-                    if (setSearchMethod != null)
-                    {
-                        setSearchMethod.Invoke(hierarchyWindow, new object[] {""});
-                        Debug.Log("Hierarchy search field cleared.");
-                    }
-                    else
-                    {
-                        Debug.LogError("Unable to find SetSearch method.");
-                    }
-                }
-            }
-            else
-            {
-                Debug.LogError("Unable to find SceneHierarchyWindow type.");
-            }
+            Debug.LogError("Unable to find SceneHierarchyWindow type.");
         }
     }
 }
