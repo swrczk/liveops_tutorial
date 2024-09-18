5f128088c8fb5d2986eb32d72ec3d6f7e3ef20a2
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2024-09-18 16:08:44 +0200

    game - zadania

---
+++ Assets/Scripts/Editor/AspectRatioCriterion.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using Unity.Tutorials.Core.Editor;
+using UnityEditor;
+
+namespace Editor
+{
+#if UNITY_EDITOR
+    public class AspectRatioCriterion : Criterion
+    {
+        [SerializeField]
+        private int aspectWidth = 9;
+
+        [SerializeField]
+        private int aspectHeight = 16; 
+
+        public void OnEnable()
+        {
+            EditorApplication.update += UpdateCompletion;
+        }
+
+        public void OnDisable()
+        {
+            EditorApplication.update -= UpdateCompletion;
+        }
+
+        public override bool AutoComplete()
+        {
+            return false;
+        }
+
+        protected override bool EvaluateCompletion()
+        {
+            return IsAspectRatioApplied();
+        }
+
+
+        private bool IsAspectRatioApplied()
+        {
+            var aspectRatio = (float) Screen.width / Screen.height;
+            var expectedAspectRatio = (float) aspectWidth / aspectHeight;
+            return Math.Abs(aspectRatio - expectedAspectRatio) < 0.01f;
+        } 
+    }
+#endif
+}
---
+++ Assets/Scripts/Editor/ResolutionCriterion.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using Unity.Tutorials.Core.Editor;
+using UnityEditor;
+
+namespace Editor
+{
+#if UNITY_EDITOR
+    public class ResolutionCriterion : Criterion
+    {
+        [SerializeField]
+        private int width = 1080;
+
+        [SerializeField]
+        private int height = 1920 ; 
+
+        public void OnEnable()
+        {
+            EditorApplication.update += UpdateCompletion;
+        }
+
+        public void OnDisable()
+        {
+            EditorApplication.update -= UpdateCompletion;
+        }
+
+        public override bool AutoComplete()
+        {
+            return false;
+        }
+
+        protected override bool EvaluateCompletion()
+        {
+            return IsAspectRatioApplied();
+        }
+
+
+        private bool IsAspectRatioApplied()
+        {
+            return Screen.width == width && Screen.height == height;
+        } 
+    }
+#endif
+}
